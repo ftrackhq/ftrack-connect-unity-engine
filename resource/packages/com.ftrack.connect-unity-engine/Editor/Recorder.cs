cbb8e498e083ec7b8febf9b7e7151c3b676b9ce0
vkovec <viktoria.kovecses@gmail.com>
2019-07-10 14:44:23 -0400

    map frame rate to correct enum type

--- resource/packages/com.ftrack.connect-unity-engine/Editor/Recorder.cs
+++ resource/packages/com.ftrack.connect-unity-engine/Editor/Recorder.cs
@@ -16,13 +16,30 @@ namespace UnityEditor.ftrack
             var settings = field.GetValue(window) as RecorderControllerSettings;
             settings.SetRecordModeToFrameInterval(start, end);
             
-            //////////// TODO ////////////////
-            /// Use the mapping algorithm discussed on Slack
-            /// Set the FramerateType to the right enum, or set a value 
-            /// It will switch the FramerateType to CUSTOM if I am correct.
-            /// (see RecorderControllerSettings.cs)
-            settings.frameRate = fps;
-            //////////// TODO ////////////////
+            // Get the dictionary of frame rate options to float values
+            var frameRateDictField = settings.GetType().GetField("s_FPSToValue", BindingFlags.NonPublic | BindingFlags.Static);
+            var frameRateDict = frameRateDictField.GetValue(null) as System.Collections.IDictionary;
+
+            // Get the frame rate type field to set with the appropriate enum value
+            var frameRateTypeField = settings.GetType().GetField("m_FrameRateType", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            bool setValue = false;
+            foreach (System.Collections.DictionaryEntry keyValuePair in frameRateDict)
+            {
+                float fpsRounded = (float)System.Math.Round(fps, 2);
+                float value = (float)System.Math.Round((float)keyValuePair.Value, 2);
+                if(fpsRounded == value)
+                {
+                    frameRateTypeField.SetValue(settings, keyValuePair.Key);
+                    setValue = true;
+                    break;
+                }
+            }
+
+            if (!setValue)
+            {
+                settings.frameRate = fps;
+            }
 
             // Now apply the window settings to all recorders and save the new global settings
             var onGlobalSettingsChanged = window.GetType().GetMethod("OnGlobalSettingsChanged", BindingFlags.NonPublic | BindingFlags.Instance);
